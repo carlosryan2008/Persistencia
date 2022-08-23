1554d8fbaa8db3234aa9ffeb5e1b9515a3d84c4b
carlosryan2008 <48930497+carlosryan2008@users.noreply.github.com>
2022-08-23 16:10:23 -0300

    Update Leitor.java

--- Leitor.java
+++ Leitor.java
@@ -1,4 +1,4 @@
-package leitor;
+
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -10,19 +10,25 @@ import java.util.Scanner;
 
 public class Leitor {
 
-	public static void main(int[] args) throws IOException{
+	public static void main(String[] args) throws IOException{
+		//Leitor do teclado
 		System.out.println("Digite o caminho do arquivo");
 		Scanner ler = new Scanner(System.in);
 		String caminho = ler.nextLine();
 		
+		
 		InputStream is = new FileInputStream(caminho);
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
-		String s;
+		String s = br.readLine();
+		int cont = 0;
+		int n1 = Integer.parseInt(args[0]);
+		int n2 = Integer.parseInt(args[1]);
 		
-		int n1 = args[0];
-		int n2 = args[1];
-		for(int x = 0; x < n2; x++) {
+		// Primeira versão
+		//int n1 = 2;
+		//int n2 = 6;
+		/*for(int x = 0; x < n2; x++) {
 			s = br.readLine();
 			if(x < n1-1) {
 				continue;
@@ -30,12 +36,19 @@ public class Leitor {
 			else{
 				System.out.println(s);
 			}
-		}
+		}*/
 		
+		// Versão onde n1 <= 0 e n2 <= 0 representam a não definição
+		while(s != null && cont < n2 || s != null && n2 < 0) {
+			if(cont >= n1-1) 	System.out.println(s);
+			cont++;
+			s = br.readLine();
+		}
 		
 		
 		br.close();
 		ler.close();
+		
 	}
 
 }
